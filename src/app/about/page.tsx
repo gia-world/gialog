042195b939d789cecb0d872820b1e29fdaa58ef1
Gia <gia.jihye.j@gmail.com>
2023-11-03 15:14:03 +0700

    초기 세팅

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Profile from "../components/Profile";
+
+export default function AboutPage() {
+  return (
+    <>
+      <Profile />
+      <div>설명</div>
+    </>
+  );
+}
