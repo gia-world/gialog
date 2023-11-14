509ae0c788ab4ca32b3e0807c984cff98326a0ba
Gia <gia.jihye.j@gmail.com>
2023-11-14 18:20:38 +0700

    route return 추가

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,4 @@
 import { fsCreatePost, fsGetPostsList } from "@/service/posts";
-
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
@@ -10,5 +9,5 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
 export async function POST(req: NextApiRequest, res: NextApiResponse) {
   const data = JSON.parse(req.body);
   const result = await fsCreatePost(data);
-  res.status(200).json(result);
+  return res.status(200).json(result);
 }
