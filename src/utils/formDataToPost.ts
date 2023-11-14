f27dc656efa2a1e9fc5c416c3ffae0f48bbbf1e2
Gia <gia.jihye.j@gmail.com>
2023-11-14 22:32:40 +0700

    타입 오타 수정

--- src/utils/formDataToPost.ts
+++ src/utils/formDataToPost.ts
@@ -1,11 +1,11 @@
-import { CreatePost } from "@/types/post";
+import { NewPost } from "@/types/post";
 
-export default function formDataToPost(formData: FormData): CreatePost {
+export default function formDataToPost(formData: FormData): NewPost {
   const post: any = {};
   formData.forEach((value, key) => {
     if (["title", "desc", "tag", "content"].includes(key)) {
       post[key] = key === "tag" ? (value as String).split(",") : value;
     }
   });
-  return post as CreatePost;
+  return post as NewPost;
 }
