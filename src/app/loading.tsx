03f2e5414265a6fb2b509fabe180dcf88609307a
Gia <gia.jihye.j@gmail.com>
2023-11-27 22:27:19 +0900

    loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,10 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Loading() {
-  return <p className="italic">Loading...</p>;
+  return (
+    <div className="absolute top-0 left-0 w-screen h-screen bg-black bg-opacity-50 flex justify-center items-center">
+      <Image src="/loading.gif" alt="loading..." width={80} height={80} />
+    </div>
+  );
 }
